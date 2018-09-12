fb7a93561b12af5d91748fab0573bc9bfa8cfc31
Pedro Teixeira <pedroteixeirabisognin@gmail.com>
2018-09-12 18:26:01 -0300

    Inserindo novos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PainelComponent } from './painel/painel.component';
 import { TopoComponent } from './topo/topo.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PainelComponent,
-    TopoComponent
+    TopoComponent,
+    TentativasComponent,
+    ProgressoComponent
   ],
   imports: [
     BrowserModule
