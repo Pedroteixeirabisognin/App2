95293e71d1749cd9a9179319c39d001557447f5f
Pedro Teixeira <pedroteixeirabisognin@gmail.com>
2018-09-12 18:08:04 -0300

    corrigindo problema de 0 null

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PainelComponent } from './painel/painel.component';
+import { TopoComponent } from './topo/topo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelComponent,
+    TopoComponent
   ],
   imports: [
     BrowserModule
