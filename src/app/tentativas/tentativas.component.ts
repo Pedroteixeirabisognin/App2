aaea2d10e9631469395c5861ffc52f515ca6e218
Pedro Teixeira <pedroteixeirabisognin@gmail.com>
2018-09-13 21:42:13 -0300

    Metodo ngOnChanges

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy, OnChanges } from '@angular/core';
 import {Coracao} from '../shared/coracao.model';
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
+export class TentativasComponent implements OnInit,OnChanges {
 
   
   public coracoes: Array<Coracao> = new Array(new Coracao(true),new Coracao(true),new Coracao(true))
@@ -19,10 +19,12 @@ export class TentativasComponent implements OnInit {
   }
 
   ngOnInit() {
+
+  }
+  //É executado na alteração de valores de componentes pais para componentes filhos
+  ngOnChanges(): void {
     console.log('Tentativas restantes: ', this.tentativas);
 
   }
-
-
 
 }
