ffa773d9e4f984cc9b9093a71457db3fa3516c60
Pedro Teixeira <pedroteixeirabisognin@gmail.com>
2018-09-14 01:10:37 -0300

    Diretiva *ngIf e onDestroy

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
 import {Frase} from '../shared/frase.model';
 import {FRASES} from './frases-mock';
 import { Alert } from 'selenium-webdriver';
@@ -7,7 +7,8 @@ import { Alert } from 'selenium-webdriver';
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css']
 })
-export class PainelComponent implements OnInit {
+export class PainelComponent implements OnInit,OnDestroy {
+
   public frases: Array<Frase> = FRASES
   public instrucao:string = 'Traduza a frase'
   public resposta:string  = ''
@@ -48,6 +49,10 @@ export class PainelComponent implements OnInit {
     }
   }
   
+  ngOnDestroy(): void {
+    console.log('Painel foi destruido')
+    
+  }
   
   public atualizarRodada():void {
     this.rodadaFrase =this.frases[this.rodada]
